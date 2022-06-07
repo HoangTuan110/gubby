e22ba5e258adb99dc19bd6206a8540ebe3f19af9
Dang Hoang Tuan (Wumi4) <danghoangtuan526@gmail.com>
2022-06-07 21:12:19 +0700

    Add secret key (because flask needs it for some reason) and also fix the error handling when an non-existent user is requested

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from httpx import get
 from json import loads
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "265ceaf4cf562ec49f3ad1b187eaa84946b4e2b3090548a3d028a4a3c3a86322"
 
 templates = TemplateLookup(directories=["./templates"], module_directory="/tmp/mako_modules")
 def serve_template(name, **kwargs):
@@ -17,15 +18,15 @@ def index():
 @app.route("/<user>")
 def get_user(user):
     user_json = loads(get(f"https://api.github.com/users/{user}").text)
-    # If the user's JSON contains a message key, i.e. request has failed,
+    # If the user's JSON doesn't contain a `login` key, i.e. request has failed,
     # We will flash the error message back to the user
-    # If it doesn't exist, we will display content as usual
+    # If it does exist, we will render content as usual
     try:
-        user_json["message"]
-        flash(f"Error when requesting user: {user_json['message']}")
-        return redirect(url_for("/"))
-    except Exception as _:
+        _ = user_json["login"]
         return serve_template("user.html", user=user_json)
+    except KeyError:
+        flash(f"Error when requesting user: {user_json['message']}")
+        return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
