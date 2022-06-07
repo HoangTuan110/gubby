7c1b38e627a17348a6f0b667ced051ee637fae53
Dang Hoang Tuan (Wumi4) <danghoangtuan526@gmail.com>
2022-06-07 07:53:51 +0700

    Add user request first

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, flash, get_flashed_messages, redirect, url_for
 from mako.lookup import TemplateLookup
 from httpx import get
 from json import loads
@@ -12,7 +12,20 @@ def serve_template(name, **kwargs):
 
 @app.route("/")
 def index():
-    return serve_template("index.html", host_url=request.host_url)
+    return serve_template("index.html", host_url=request.host_url, messages=get_flashed_messages())
+
+@app.route("/<user>")
+def get_user(user):
+    user_json = loads(get(f"https://api.github.com/users/{user}").text)
+    # If the user's JSON contains a message key, i.e. request has failed,
+    # We will flash the error message back to the user
+    # If it doesn't exist, we will display content as usual
+    try:
+        user_json["message"]
+        flash(f"Error when requesting user: {user_json['message']}")
+        return redirect(url_for("/"))
+    except Exception as _:
+        return serve_template("user.html", user=user_json, host_url=request.host_url)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
